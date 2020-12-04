1599e2c1c5035a0e3f80659a8d64bb4458420e29
Darioorfano <darioorfano@hotmail.com>
2020-12-04 20:06:24 -0300

    Se agrego archivo app.js

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,7 @@ var contraseña= document.getElementById("inputPassword").value;
 
 
 /*Registrar Usuarios nuevos*/ 
-
-firebase.auth().createUserWithEmailAndPassword(email, contraseña)
+firebase.auth().createUserWithEmailAndPassword(email,contraseña)
   .then((user) => {
     // Signed in
     // ...
@@ -17,3 +16,39 @@ firebase.auth().createUserWithEmailAndPassword(email, contraseña)
   });
 
 }
+
+function ingreso (){
+
+var emailRegistro=document.getElementById("emailRegistro").value;
+var contraseñaRegistro=document.getElementById("contraseñaRegistro").value;
+
+
+    firebase.auth().signInWithEmailAndPassword(emailRegistro,contraseñaRegistro)
+    .then((user) => {
+      // Signed in
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+
+}
+function datosUsuarios(){
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+       console.log("existe usuario");
+            var displayName=user.displayName;
+       var email=user.email;
+       var emailVerified=user.emailVerified;
+       var photoURL=user.photoURL;
+       var isAnonymous= user.isAnonymous;
+       var uid = user.uid;
+        var providerData=user.providerData;
+        } else {
+          // User is signed out
+          console.log("no existe usuario");
+        }
+      });
+}
+datosUsuarios();
