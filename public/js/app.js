4bebe24186ecf1c72b40fe24632b2e3a76b10f91
Darioorfano <darioorfano@hotmail.com>
2020-12-27 22:01:44 -0300

    Se agrego funcion para que el usuario pueda cambiar el fondo de perfil

--- public/js/app.js
+++ public/js/app.js
@@ -1,139 +1,111 @@
 
-var btnLogin = document.getElementById("btnLoginGoogle");
-var btnLogout = document.getElementById("btnLogoutGoogle");
+var btnLoginGoogle = document.getElementById("btnLoginGoogle");
 
+var btnLogout = document.getElementById("btnLogout");
+
+var fotoPerfil=document.getElementById("fotoPerfil");
+
+var nombreUsuario=document.getElementById("nombreUsuario");
+
+var btnLoginFacebook=document.getElementById("btnLoginFacebook");
+
+
+
+var btnCustomBackground=document.getElementById("btnCustomBackground");
+
+var miCuenta=document.getElementById("miCuenta");
 // Para verificar si hay usuario activo
 firebase.auth().onAuthStateChanged(function(user){
     console.log(user)
     if (user) {
-        console.log("tenemos usuario");
+        console.log("Se encuentra conectado");
+        
+        var displayName= user.displayName;
+        var email= user.email;
+        var emailVerified= user.emailVerified;
+        var photoURL= user.photoURL;
+      
+        var isAnonymous= user.isAnonymous;
+        var uid=user.uid;
+        var providerData=user.providerData;
+        
+        fotoPerfil.innerHTML=`<img src=${photoURL} alt="foto de perfil">`;
+       nombreUsuario.innerHTML=`<p>Bienvenido ${displayName}</p>`;
+       nombreUsuario.style.color="white";
+        
         mostrarCerrar();
     }else{
-        console.log("no tenemos usuario");
+        console.log("No se ha encontrado el usuario");
+        miCuenta.style.display="none";
         mostrarIniciar();
     }
 });
 
+
 //Inicio de sesion datosDelUsuario=result
-btnLogin.addEventListener("click", function(){
-    //event.preventDefault();  evita que se recargue la pag
-    var provider = new firebase.auth.GoogleAuthProvider();
-  
-    firebase.auth().signInWithPopup(provider)
-    .then(function(datosUsuario){
-        console.log(datosUsuario)
-        window.location.replace("https://xtremegaming.firebaseapp.com/miCuenta.html");
-      
-    }).catch(function(err){
-        console.log(err)
-    })
-});
+if(btnLoginFacebook){
+    btnLoginFacebook.addEventListener("click",function(){
+        var provider = new firebase.auth.FacebookAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+        .then(function(datosUsuario){
+            console.log(datosUsuario)
+            window.location.replace("http://127.0.0.1:5500/public/miCuenta.html"); 
+           // window.location.replace("/miCuenta.html");
+           //Cuando termine cambiar el replace asi  /miCuenta.html// 
+        }).catch(function(err){
+            console.log(err)
+        })
+    
+    
+    });
 
-//Cierre de sesion
-btnLogout.addEventListener("click", function(){
-    //event.preventDefault();  evita que se recargue la pag
-    firebase.auth().signOut().then(function(){
-        alert("se ha cerrado la sesion")
-    })
-})
-/*
-var user = firebase.auth().currentUser;
-var name, email, photoUrl, uid, emailVerified;
-
-if (user != null) {
-  name = user.displayName;
-  email = user.email;
-  photoUrl = user.photoURL;
-  
-  emailVerified = user.emailVerified;
-  uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-                   // this value to authenticate with your backend server, if
-                   // you have one. Use User.getToken() instead.
 }
-*/
-//Funciones para mostrar y ocultar botones
-/*
+if(btnLoginGoogle){
+    btnLoginGoogle.addEventListener("click", function(){
+        //event.preventDefault();  evita que se recargue la pag
+            var provider = new firebase.auth.GoogleAuthProvider();
+      
+            firebase.auth().signInWithPopup(provider)
+            .then(function(datosUsuario){
+                console.log(datosUsuario)
+                window.location.replace("http://127.0.0.1:5500/public/miCuenta.html"); 
+                
+                //window.location.replace("/miCuenta.html");
+               /*Cuando termine cambiar el replace asi  /miCuenta.html*/ 
+            }).catch(function(err){
+                console.log(err)
+            })
+    
+    
+    });
+}
+
+if(btnLogout){
+    btnLogout.addEventListener("click",function(){
+        firebase.auth().signOut().then(function() {
+            // Sign-out successful.
+               console.log("Se ha desconectado Correctamente");
+                window.location.replace("http://127.0.0.1:5500/public/index.html");
+          }).catch(function(error) {
+            // An error happened.
+          })
+    
+    
+    });
+
+}
+
+
+   
+
 function mostrarCerrar(){
     btnLogin.style.display="none";
     btnLogout.style.display="block";
+    btnCustomBackground.style.display="flex";
 }
 
 function mostrarIniciar(){
     btnLogout.style.display="none";
     btnLogin.style.display="block";
+    btnCustomBackground.style.display="none";
 }
-*/
-
-
-
-
-
-
-/*
-
-function IngresoConGoogle(){
-var provider= new firebase.auth.GoogleAuthProvider();
-
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  console.log(user);
-  window.location.replace("https://xtremegaming.firebaseapp.com/miCuenta.html");
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-
-});
-
-
-firebase.auth().signOut().then(function() {
-  // Sign-out successful.
-}).catch(function(error) {
-  // An error happened.
-});
-
-}
-
-function  IngresoConFacebook(){
-  var provider = new firebase.auth.FacebookAuthProvider();
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    console.log(user);
-    window.location.replace("https://xtremegaming.firebaseapp.com/miCuenta.html");
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-  
-  
-  firebase.auth().signOut().then(function() {
-    // Sign-out successful.
-  }).catch(function(error) {
-    // An error happened.
-  });
-
-
-}
-
-
-    */
-
-
-
